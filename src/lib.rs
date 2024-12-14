75a5ae91015d328fc26dc1520a68dcafe755129e
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-14 17:17:46 -0500

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+uniffi::include_scaffolding!("math");
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn add(a: u32, b: u32) -> u32 {
+    a + b
 }
